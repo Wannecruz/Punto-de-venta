4b5fda9ba4840cf6e5065d687a0b6caab8db9023
Wannecruz <whernandezc10@miumg.edu.gt>
2022-10-18 23:18:14 -0600

    Nbar ventas ralizadas

--- inc/navbar.php
+++ inc/navbar.php
@@ -14,7 +14,7 @@ if (!isset($_SESSION)) exit("<script>window.location.href = '../';</script>");
         </div>
         <div id="navbar" class="navbar-collapse collapse">
             <ul class="nav navbar-nav">
-                <li id="elem_ventas"><a href="./ventas"><i class="fa fa-money"></i> Ventas</a></li>
+                <li id="elem_ventas"><a href="./ventas"><i class="fa fa-money"></i> Ventas Realizadas</a></li>
                 <li id="elem_inventarios"><a href="./inventarios"><i class="fa fa-book"></i> Inventarios</a></li>
                 <li id="elem_caja"><a href="./caja"><i class="fa fa-money"></i> Caja123</a></li>
                 <li id="elem_gastos"><a href="./gastos"><i class="fa fa-calculator"></i> Gastos</a></li>
